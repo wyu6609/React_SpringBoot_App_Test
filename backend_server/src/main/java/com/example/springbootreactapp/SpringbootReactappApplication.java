1b62f2f5babe2581bd816521ddb5bd36a8ace346
Will <wy427@nyu.edu>
2023-01-14 06:36:49 -0500

    initial commit

---
+++ backend_server/src/main/java/com/example/springbootreactapp/SpringbootReactappApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootreactapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactappApplication.class, args);
+	}
+
+}
